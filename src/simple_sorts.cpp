d7d0580d3ee21a7bccbf79c9b3964f0e12ced51b
Lefterache Ștefan <lefterache.stefan.8@gmail.com>
2026-05-20 14:17:56 +0300

    make postincrement great again!

--- src/simple_sorts.cpp
+++ src/simple_sorts.cpp
@@ -4,9 +4,9 @@ namespace sorting {
 
 void bubble_sort(std::vector<int>& a) {
 	const int n = a.size();
-	for (int i = 0; i < n - 1; ++i) {
+	for (int i = 0; i < n - 1; i++) {
 		bool swapped = false;
-		for (int j = 0; j < n - i - 1; ++j) {
+		for (int j = 0; j < n - i - 1; j++) {
 			if (a[j] > a[j + 1]) {
 				std::swap(a[j], a[j + 1]);
 				swapped = true;
@@ -20,7 +20,7 @@ void bubble_sort(std::vector<int>& a) {
 
 void insertion_sort(std::vector<int>& a) {
 	const int n = a.size();
-	for (int i = 1; i < n; ++i) {
+	for (int i = 1; i < n; i++) {
 		const int key = a[i];
 		int j = i - 1;
 		while (j >= 0 && a[j] > key) {
